f3cdfc6b20073c319e95e07b1dc69f62be49a204
TBoileau <you@example.com>
2020-04-25 14:48:49 +0200

    style(flash-bag): Add flash bag component
    Install webpack-encore.
    Add sass and typescript.
    Create some scss files.
    Implement flash effect in js.

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/js/app.js')
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+    .enableSassLoader()
+    .enableTypeScriptLoader()
+    .copyFiles({
+        from: './assets/images'
+    })
+;
+
+module.exports = Encore.getWebpackConfig();
